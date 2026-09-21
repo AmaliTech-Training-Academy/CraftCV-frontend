fa225c8a7d9b93da88129fa8fc9082960b4b48d5
OEMBA <emmba67@gmail.com>
2026-09-21 11:26:46 +0000

    fix(review): update font imports and clsx dependencies

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -2,7 +2,7 @@
 import type { HTMLAttributes } from "vue";
 import type { ButtonVariants } from ".";
 import { Primitive } from "reka-ui";
-import { cn } from "~/lib/utils";
+import { cn } from "@/lib/utils";
 import { buttonVariants } from ".";
 
 interface Props {
